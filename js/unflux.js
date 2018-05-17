b48cb648693d6a60f55e94ca0e7b20843f7dac19
Aurélien Ravier <aurelien.ravier@unflux.fr>
2018-05-17 11:17:02 +0200

    Update unflux.js

--- js/unflux.js
+++ js/unflux.js
@@ -7,35 +7,36 @@
             this.popin();
             this.addLang();
             this.showMenu();
+            this.scrollHorizontalTable();
 
         },
         
 
         popin : function(){
-            $(".video-item").click(function(){
+            $('.video-item').click(function(){
                 var videoName = $(this).data('videoname');
-                $("#"+videoName).addClass('popin-open');
-                $("html").addClass('noscroll');
+                $('#'+videoName).addClass('popin-open');
+                $('html').addClass('noscroll');
             });
             $(".popin-close, .popin-overlay").click(function(){
                 var videoName = $(this).data('videoname');
-                $(".popin").removeClass('popin-open');
-                $("#"+ videoName +' iframe').attr("src", $("#"+ videoName +' iframe').attr("src"));
-                $("html").removeClass('noscroll');
+                $('.popin').removeClass('popin-open');
+                $("#"+ videoName +' iframe').attr('src', $('#'+ videoName +' iframe').attr('src'));
+                $('html').removeClass('noscroll');
             });
         },
 
         addLang : function() {
-            $(".js .ls-lang-list").append('<li class="ls-lang-item ls-lang-es"><a class="ls-lang-link" href="#"><abbr title="Español">es</abbr></a></li> <li class="ls-lang-item ls-lang-it"><a class="ls-lang-link" href="#"><abbr title="Italiano">it</abbr></a></li>');
+            $('.js .ls-lang-list').append('<li class="ls-lang-item ls-lang-es"><a class="ls-lang-link" href="#"><abbr title="Español">es</abbr></a></li> <li class="ls-lang-item ls-lang-it"><a class="ls-lang-link" href="#"><abbr title="Italiano">it</abbr></a></li>');
         },
 
 
         showMenu : function() {
-            var menuBtn = $(".sb-menu-trigger");
-            var mainNav = $(".site-banner > .inside .main-navigation");
+            var menuBtn = $('.sb-menu-trigger');
+            var mainNav = $('.site-banner > .inside .main-navigation');
 
             menuBtn.click(function(){
-                if(mainNav.hasClass("menu-open")) {
+                if(mainNav.hasClass('menu-open')) {
                     mainNav.removeClass('menu-open');
                     menuBtn.removeClass('menu-open');
                 } else {
@@ -43,6 +44,10 @@
                     menuBtn.addClass('menu-open');
                 }
             });
+        },
+
+        scrollHorizontalTable : function() {
+            $('.article-content table').wrap('<div class="horizontalScroll"></div>');
         }
 
     }
