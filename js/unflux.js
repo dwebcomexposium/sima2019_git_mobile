7bc2b27aed64a5e6686c2255528450c82fef136a
Aurélien Ravier <aurelien.ravier@unflux.fr>
2022-12-09 10:25:20 +0100

    Update unflux.js

--- js/unflux.js
+++ js/unflux.js
@@ -91,17 +91,17 @@ const getCookie = (cname) => {
                 dateSection.classList.add('salon-dates')
                 dateSection.innerHTML = `
                 <div class="salon-dates__start salon-dates__part">
-                    <p class="salon-dates__day">06</p>
+                    <p class="salon-dates__day">24</p>
                     <p class="salon-dates__month">novembre</p>
-                    <p class="salon-dates__year">2022</p>
+                    <p class="salon-dates__year">2024</p>
                     <svg viewBox="0 0 46 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 11.3333C8.94552 11.3333 11.3333 8.94552 11.3333 6C11.3333 3.05448 8.94552 0.666667 6 0.666667C3.05448 0.666667 0.666667 3.05448 0.666667 6C0.666667 8.94552 3.05448 11.3333 6 11.3333ZM6 7L46 7V5L6 5V7Z" fill="#252525"/>
                     </svg>
                 </div>
                 <div class="salon-dates__end salon-dates__part">
-                    <p class="salon-dates__day">10</p>
+                    <p class="salon-dates__day">28</p>
                     <p class="salon-dates__month">novembre</p>
-                    <p class="salon-dates__year">2022</p>
+                    <p class="salon-dates__year">2024</p>
                     <svg viewBox="0 0 41 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M40.7071 8.70711C41.0976 8.31658 41.0976 7.68342 40.7071 7.29289L34.3431 0.928932C33.9526 0.538408 33.3195 0.538408 32.9289 0.928932C32.5384 1.31946 32.5384 1.95262 32.9289 2.34315L38.5858 8L32.9289 13.6569C32.5384 14.0474 32.5384 14.6805 32.9289 15.0711C33.3195 15.4616 33.9526 15.4616 34.3431 15.0711L40.7071 8.70711ZM0 9L40 9V7L0 7L0 9Z" fill="white"/>
                     </svg>
